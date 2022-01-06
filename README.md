f5e9e3b79291020aae59345843be704c3ccee10c
SohamNandy2006 <66298183+SohamNandy2006@users.noreply.github.com>
2022-01-06 22:06:39 +0530

    qol changes

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ZoomAutoJoiner
 A simple program I wrote that autojoins zoom links without having to open the browser.
+
+## Update
+Added logging facility to monitor script when running in background. Binaries of the same have been built.
+
 ## Usage
 Either run the script with `python3 zoomautojoiner.foreground` or `pythonw zoomautojoiner.background` or run the EXE file.
 If you are on on a POSIX system use either `python3 zoomautojoiner.foreground` or `sudo nohup python3 zoomautojoiner.foreground` to run in 
@@ -11,5 +15,6 @@ Run with `-verbose` argument to show extra information. I use this to see whethe
 Python3 and nothing else alternatively you can natively run this on windows with the binary.The script should work on both windows and POSIX systems. 
 ## Binaries
 .exe (recommended for windows users as it doesnt pause out of focus) binaries are available in the `binaries` folder. The folder also contains the SHA256 and MD5 checksum of the file. Be sure to verify the files before running.
-## Notes
-If you find a bug raise it on github. Also see if I have already patched it because the script doesnt auto update.
+## Notes for noobs
+Run the installdependancy.bat file before running the script. If it shows a error check your python installation and ensure that 
+`pip` is accessible through the shell. If for some reason you are unable to run the script use the binaries provided.
--- zoomautojoin.py
+++ zoomautojoin.py
@@ -1,13 +1,28 @@
-from time import sleep, strftime
-import os,sys
-timeIndex = []
-linkIndex = []
 import logging
-logging.basicConfig(filename="log_BackGround.txt", level=logging.INFO,format="%(asctime)s %(message)s")
+from sys import platform
 
+Windows = False
+if platform == "win32" or platform == "cygwin":
+    Windows = True
+
+logging.basicConfig(filename="log.txt", level=logging.INFO,format="%(asctime)s %(message)s")
 def Print(toPrint):
     logging.info("{}".format(toPrint))
     print(toPrint)
+from time import sleep, strftime
+import os,sys
+
+if Windows:
+    try:
+        import subprocess
+    except:
+        Print("Subprocess module cannot be imported. Did you install the dependancy ??")
+        quit(0)
+
+timeIndex = []
+linkIndex = []
+descIndex = []
+
 
 def parseFile():
     timeIndex.clear()
@@ -16,13 +31,23 @@ def parseFile():
     meetingLines = source.readlines()
     indexcounter = 0
     for line in meetingLines:
-        a= line.split(" ")
+        a = line.split(" ",2)
         timeIndex.append("placeholder")
         linkIndex.append("placeholder")
+        descIndex.append("placeholder") 
         timeIndex[indexcounter] = a[0]
         linkIndex[indexcounter] = a[1]
+        if (len(a) ==3):
+            if a[2].strip() == "" or a[2].strip() == " ":
+                descIndex[indexcounter] = "No description provided"
+            else:
+                descIndex[indexcounter] = a[2]
+        else:
+            print(len(a))
+            descIndex[indexcounter] = "No description provided"
         indexcounter+= 1
-      
+    source.close()
+
 def main():
     print("Rest easy... this will open zoom when the time comes UwU")
     try:
@@ -36,7 +61,7 @@ def main():
         currenttime = strftime("%H:%M")
         for time in timeIndex:
             if verboseFlag == True:
-                print("currenttime = {} and comparing to {}".format(currenttime,time))
+                print("Currenttime = {} and comparing to {}".format(currenttime,time))
             if currenttime == time:
                 index = timeIndex.index(time)
                 # using zoombie logic to open zoom without browser thanks to https://github.com/DaBigBlob/zoombie
@@ -44,10 +69,10 @@ def main():
                 domain = link.split('/')[3]
                 mID = link.split('/')[-1].split('?')[0]
                 pswd = link.split('/')[-1].split('=')[1]
-                os.system(f'start zoommtg://{domain}/join?action=join"&"confno={mID}"&"pwd={pswd} /HIGH')
-                
-                print("JOINING MEETING")
-                print("sleeping for 1 minute to avoid ACCIDENTAL meeting join")
+
+                os.system(f'start zoommtg://{domain}/join?action=join"&"confno={mID}"&"pwd={pswd} /HIGH >NUL  2>NUL')    
+                Print(f"JOINING MEETING: {linkIndex[index]} - DESC: {descIndex[index]}")
+                Print("Sleeping for 60 seconds to avoid accidental meeting join")
                 sleep(60)
                 parseFile()
         sleep(5)
